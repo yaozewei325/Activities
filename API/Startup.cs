2baf77f020d94fb83bf221d742edbf84cb960172
yaozewei325 <2197603@cegepoutaouais.qc.ca>
2022-11-28 13:33:42 -0500

    add react skeleton section3

--- API/Startup.cs
+++ API/Startup.cs
@@ -27,6 +27,14 @@ namespace API
             {
                 opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +50,7 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
---
+++ client-app/src/demo.ts
@@ -0,0 +1,8 @@
+const duck1 = {
+
+    name: 'heu',
+    numLegs: 2,
+    makeSound:(sound:any)=>console.log(sound)
+
+}
+ 
